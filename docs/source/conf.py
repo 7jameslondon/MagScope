db9ddb20d6868a5111b10139fc29f3a21782f400
James London <7jameslondon@gmail.com>
2025-11-05 15:30:04 -0500

    Enhanced Sphinx documentation configuration: added extensions, type hints, autoapi, and updated theme with repository integration.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,42 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 project = 'MagScope'
 copyright = '2025, James London'
 author = 'James London'
 release = '0.1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+    "sphinx_design",
+    'autoapi.extension',
+]
 
-extensions = []
+autoapi_type = "python"
+autoapi_dirs = ["../../magscope"]
+autoapi_add_toctree_entry = True
+autoapi_root = "api"
+
+autodoc_mock_imports = ["cupy"]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+html_logo = "../../assets/logo.png"
+
+html_theme_options = {
+    "repository_url": "https://github.com/7jameslondon/MagScope",
+    "use_repository_button": True,
+    "path_to_docs": "docs/source",
+}
+
+autosummary_generate = True
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
