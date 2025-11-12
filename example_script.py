e7f518360835fa75c60f9ee9578f248c77a0dfde
James London <7jameslondon@gmail.com>
2025-11-12 15:53:43 -0500

    Updated the example_script.py

--- example_script.py
+++ example_script.py
@@ -4,7 +4,18 @@ import magscope
 from magscope import Units, AcquisitionMode
 
 my_script = magscope.Script()
+
+# Change the acquisition
+my_script('sleep', 2.*Units.sec)
 my_script('set_acquisition_mode', AcquisitionMode.CROP_VIDEO)
+my_script('set_acquisition_on', False)
+my_script('sleep', 2.*Units.sec)
 my_script('set_acquisition_on', True)
-my_script('set_acquisition_dir_on', True)
-my_script('set_acquisition_dir', r'C:\Users\lond11\Documents\MagScope and MagTrack\temp')
+my_script('set_acquisition_mode', AcquisitionMode.TRACK)
+my_script('sleep', 2.*Units.sec)
+
+# Use a for loop and Numpy
+n = 3
+array = np.random.rand(n)
+for i in range(n):
+    my_script('print', f'A random number is: {array[i]}')
