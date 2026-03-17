447e11609a9818dc91a247e864363b2480302a20
Jamie (James) London <7jameslondon@gmail.com>
2026-03-17 18:38:50 -0400

    Simplify main_dev startup script

--- magscope/main_dev.py
+++ magscope/main_dev.py
@@ -1,14 +1,4 @@
 import magscope
-from magscope import Command, register_script_command
-from dataclasses import dataclass
-
-@dataclass(frozen=True)
-class HelloCommand(Command):
-    name: str
-
-@register_script_command(HelloCommand)
-def say_hello(name: str):
-    print(f"Hello {name}!")
 
 scope = magscope.MagScope(verbose=True)
 scope.start()
