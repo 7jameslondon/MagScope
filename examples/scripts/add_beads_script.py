96a280a2075debf36017a6a3d29518f511d58460
Jamie (James) London <7jameslondon@gmail.com>
2026-03-19 14:35:42 -0400

    Update bead count in add_beads_script to 1000 and remove seed

--- examples/scripts/add_beads_script.py
+++ examples/scripts/add_beads_script.py
@@ -1,5 +1,5 @@
 from magscope import Script
-from magscope.ipc_commands import AddRandomBeadsCommand, ShowMessageCommand
+from magscope.ipc_commands import AddRandomBeadsCommand
 
 script = Script()
-script.append(AddRandomBeadsCommand(count=100, seed=7))
+script.append(AddRandomBeadsCommand(count=1000, seed=None))
